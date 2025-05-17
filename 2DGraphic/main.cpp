f43e9d1e45f41fdd12ee15ba88cf8ccdd7cdbed7
DeskTop_LHJ <kistudentmeta5@gmail.com>
2025-05-18 00:18:41 +0900

    feat : Add 2DGraphics Lib

---
+++ 2DGraphic/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "include/GameLib/Framework.h"
+
+
+namespace GameLib {
+	void Framework::update() {
+		unsigned* vram = videoMemory();
+		static unsigned i;
+		vram[i] += i * 100;
+		i += 9973; //1ñúà»â∫ç≈ëÂÇÃëfêî
+		i %= (width() * height());
+	}
+}
